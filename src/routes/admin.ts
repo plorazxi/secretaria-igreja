d270342067c6402b9272bcd899daedf3dee9c824
plorazxi <paulo.ss.loraschi@gmail.com>
2026-02-07 23:58:43 -0300

    Criação da rota de login do administrador

---
+++ src/routes/admin.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { compare } from "bcrypt";
+import jwt from "jsonwebtoken";
+import { env } from "../env.js";
+import { validate } from "../middleware/validate.js";
+import { loginSchema } from "../interfaces/adminInterface.js";
+
+const router = express.Router();
+
+// Rota de realização de login do administrador
+router.post('/login', validate(loginSchema), async (req, res) => {
+    const { login, password } = req.body;
+    if(login !== env.ADMIN_LOGIN) {
+        return res.status(401).send({
+            msg: "Login inválido"
+        });
+    } else if (!await compare(password, env.ADMIN_PASSWORD_HASH)) {
+        return res.status(401).send({
+            msg: "Senha incorreta"
+        });
+    } else {
+        let token = jwt.sign({
+                login: login,
+                password: env.ADMIN_PASSWORD_HASH
+            }, env.SERVER_SECRETKEY, {
+                algorithm: 'HS256',
+                expiresIn: '1h'
+        });
+        return res.status(200).send({
+            msg: "Login efetuado com sucesso",
+            token: token
+        });
+    }
+});
+
+export default router;
