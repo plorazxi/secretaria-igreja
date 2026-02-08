d50ff759aa9d78fe41b833aa9bc45c2d1c4fe5b2
plorazxi <paulo.ss.loraschi@gmail.com>
2026-02-07 21:53:28 -0300

    Criação de um servidor básico

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import { env } from "./env.js";
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/', (req, res) => {
+    res.send('root route');
+});
+
+server.listen(env.SERVER_PORT, () => {
+    console.log('server on');
+})
