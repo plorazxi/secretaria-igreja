c9a26f983035d371a89d872a4c08a50cffcca695
plorazxi <paulo.ss.loraschi@gmail.com>
2026-02-07 23:57:25 -0300

    Criação de middleware para verificação com zod

---
+++ src/middleware/validate.ts
@@ -0,0 +1,16 @@
+import type { NextFunction, Request, Response } from "express";
+import type { z } from "zod";
+
+export const validate = (schema: z.ZodObject) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const result =  await schema.safeParseAsync(req.body);
+        if (!result.success) {
+            return res.status(401).send({
+                msg: "Dados inválidos",
+                error: result.error.format()
+            });
+        }
+        req.body = result.data
+        next();
+    };
+};
