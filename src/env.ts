982e1aff161f8445592d13716055c65a3e3ceaf2
plorazxi <paulo.ss.loraschi@gmail.com>
2026-02-07 23:55:49 -0300

    Adicionando variaveis de ambiente
    Variaveis de login e uma chave para realizar o jwt

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,10 @@ import 'dotenv/config';
 
 // Tipando as variaveis de ambiente
 const envSchema = z.object({
-    SERVER_PORT: z.string()
+    SERVER_PORT: z.string(),
+    ADMIN_LOGIN: z.string(),
+    ADMIN_PASSWORD_HASH: z.string(),
+    SERVER_SECRETKEY: z.string()
 });
 
 // Exportando as variaveis de ambiente
