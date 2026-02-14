cd6f661ddb97f25d77535793067248c2eebfccf5
plorazxi <paulo.ss.loraschi@gmail.com>
2026-02-14 15:59:34 -0300

    Adicionando o serviço de documentação ao servidor

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,25 @@
 import express from "express";
 import cors from "cors";
 import { createServer } from "node:http";
+import swaggerUI from "swagger-ui-express";
+import { generateOpenApiDocs } from "./lib/openApiGenerator.js";
 import { env } from "./lib/env.js";
 import admin from "./routes/admin.js";
 import users from "./routes/users.js";
 
+import "./docs/admin.docs.js";
+import "./docs/users.docs.js";
+
 const app = express();
 const server = createServer(app);
+const swaggerDocs = generateOpenApiDocs();
 
 app.use(express.json());
 app.use(cors({
     origin: '*'
 }));
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use("/admin", admin);
 
